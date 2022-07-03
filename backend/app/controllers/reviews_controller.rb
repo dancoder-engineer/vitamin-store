0897a2d60014874503c6330dd840b71b607ffce7
Dan Seminara <japaneseforgaming@gmail.com>
2022-07-03 14:39:26 -0400

    Reviews CRUD

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+
+    def index
+        reviews = Review.all
+        render json: reviews, status: 200
+    end
+
+    def show
+        review = Review.find_by(id: params[:id])
+        if review
+            render json: review, status: 200
+        else
+            render json: {errors: ["Review not found."]}
+        end
+    end
+
+    def create
+        review = Review.create(allowed)
+        render json: review, status: :created
+    end
+
+    def update
+        review = Review.find_by(id: params[:id])
+        if review
+            review.update(allowed)
+            render json: review, status: 200
+        else
+            render json: {errors: ["Review not found."]}
+        end
+
+        def destroy
+            review = Review.find_by(id: params[:id])
+            if review
+                review.destroy
+                head :no_content
+            else
+                render json: {errors: ["Review not found."]}
+            end
+        end
+
+
+        def byuser
+            reviews = Review.where(user_id: params[:id])
+            render json: reviews, status: 200
+        end
+
+    private
+
+    def allowed
+        params.permit(:reviewtext, :stars, :user_id, :item_id)
+    end
 end
